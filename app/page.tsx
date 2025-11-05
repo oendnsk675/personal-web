a682a713b1e27be677e68f16537a390ea83535a2
sayidina ahmadal qososyi <sayidinaahmadalqososyi@gmail.com>
2025-11-05 14:35:24 +0800

    wip: hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import ArcticLightsTopGlow from '@/components/arctic-lights-glow';
 import DarkDiagonalFadeGrid from '@/components/dark-diagonal-fade-grid';
 import Header from '@/components/header';
 import { Button } from '@/components/ui/button';
+import responsive_first from '@/public/images/responsive-first-design.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,9 +13,9 @@ export default function Home() {
       <Header />
 
       <main className="">
-        <section className="w-full h-screen flex justify-between items-center">
-          <div className="w-full lg:w-1/2 grid gap-8">
-            <div className="w-fit p-1.5 rounded-l-3xl bg-linear-to-r from-emerald-700/70 to-transparent">
+        <section className="w-full h-screen grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <div className="col-span-1 flex flex-col justify-center gap-8">
+            <div className="w-fit h-fit p-1.5 rounded-l-3xl bg-linear-to-r from-emerald-700/70 to-transparent">
               <div className="p-1 px-3 rounded-l-[16.42px] bg-linear-to-r from-emerald-700/70 to-transparent text-xs xl:text-sm flex items-center pr-16 ">
                 <span className="w-2 h-2 rounded-full bg-[#22C55E] mr-2"></span>
                 <span>Open to work</span>
@@ -33,6 +35,23 @@ export default function Home() {
                 <Button variant={'outline'}>Learn How</Button>
                 <Button>More about me</Button>
               </div>
+            </div>
+          </div>
+          <div className="col-span-1 flex flex-col justify-center">
+            <div className="grid grid-cols-6 gap-2.5">
+              <div className="col-span-4 h-56 rounded-xl bg-linear-to-tr from-[rgba(21,89,65,0.30)] from-0% via-[rgba(40,162,117,0.30)] via-65% to-[rgba(21,89,65,0.30)] to-95% relative p-4 flex justify-center">
+                <p className="text-center">
+                  Bespoke, <br /> responsive-first design
+                </p>
+                <div className="absolute bottom-0 right-0 w-full flex justify-center">
+                  <Image src={responsive_first} alt="Logo" />
+                </div>
+              </div>
+              <div className="col-span-2 h-56 rounded-xl bg-linear-to-tr from-[rgba(21,89,65,0.30)] from-0% via-[rgba(40,162,117,0.30)] via-65% to-[rgba(21,89,65,0.30)] to-95% relative p-4 flex justify-center items-end">
+                <p className="text-center">No more hiding. Specialist SEO.</p>
+              </div>
+              <div className="col-span-2 h-56 rounded-xl bg-linear-to-tr from-[rgba(21,89,65,0.30)] from-0% via-[rgba(40,162,117,0.30)] via-65% to-[rgba(21,89,65,0.30)] to-95%"></div>
+              <div className="col-span-4 h-56 rounded-xl bg-linear-to-tr from-[rgba(21,89,65,0.30)] from-0% via-[rgba(40,162,117,0.30)] via-65% to-[rgba(21,89,65,0.30)] to-95%"></div>
             </div>
           </div>
         </section>
--- components/dark-diagonal-fade-grid.tsx
+++ components/dark-diagonal-fade-grid.tsx
@@ -4,8 +4,8 @@ export default function DarkDiagonalFadeGrid() {
       className="fixed inset-0 -z-20"
       style={{
         backgroundImage: `
-          linear-gradient(45deg, transparent 49%, hsl(119, 13%, 5%) 49%, hsl(119, 13%, 5%) 51%, transparent 51%),
-          linear-gradient(-45deg, transparent 49%, hsl(119, 13%, 5%) 49%, hsl(119, 13%, 5%) 51%, transparent 51%)
+          linear-gradient(45deg, transparent 49%, hsl(119, 13%, 8%) 49%, hsl(119, 13%, 8%) 51%, transparent 51%),
+          linear-gradient(-45deg, transparent 49%, hsl(119, 13%, 8%) 49%, hsl(119, 13%, 8%) 51%, transparent 51%)
         `,
         backgroundSize: '40px 40px',
         WebkitMaskImage:
